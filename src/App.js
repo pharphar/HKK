0ad7483befaeca6348ffbf596b6e42bb84795164
pharphar <jonasandersen87@gmail.com>
2024-12-23 13:46:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import CroquetTracker from './components/CroquetTracker';
 
+function App() {
+  return (
+    <div>
+      <CroquetTracker />
+    </div>
+  );
+}
+
+export default App;
