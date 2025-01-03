7d5431659a6d29c8716d48d7427856d3f9c7267e
pharphar <jonasandersen87@gmail.com>
2025-01-03 08:59:35 +0100

    Update game.js

--- server/models/game.js
+++ server/models/game.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const gameSchema = new mongoose.Schema({
   playerScores: [{
     player: {
@@ -23,4 +25,18 @@ const gameSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+}, {
+  timestamps: true
 });
+
+// Validate that there are exactly 4 players
+gameSchema.pre('save', function(next) {
+  if (this.playerScores.length !== 4) {
+    next(new Error('A game must have exactly 4 players'));
+  }
+  next();
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
